c0021c9ff190c3190b2113bacf50ef58e7a201c2
eandr127 <eandr127@outlook.com>
2017-03-22 22:55:21 -0400

    Fix version

--- src/com/myshopify/owcr/riologger/RIOLogger.java
+++ src/com/myshopify/owcr/riologger/RIOLogger.java
@@ -18,7 +18,7 @@ import java.util.logging.Level;
 
 public class RIOLogger {
 
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.2.0";
 
     private static RIOLogger logger;
     
